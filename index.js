be6397e999801e2cef236dbf3b76f6773188fc4f
william lee <will.lee.npc@gmail.com>
2023-09-17 22:51:04 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
+    const num1 = 31;
+    const num2 = 2;
+    const num3 = 12;
+    const num4 = 8;
+    let multiply = num1*num2;
+
+    let random = Math.floor(Math.random()*10)+1;
+    let mod = num3%num4;
+    let max = Math.max(1,2,20);
+
+/*
+    return Math.floor(Math.random()*10)+1;
+    
+
+    const num3 = 12;
+    const num4 = 8;
+    return num3%num4;
+
+    return Math.max(1,2,3,20);*/
+
 // Write your code here
